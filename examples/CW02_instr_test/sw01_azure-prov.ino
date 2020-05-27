61fcf0c439b4adb147689e91a4faeb23a7b87d65
KalbeAbbas <kalbeabbas142@gmail.com>
2020-05-27 21:04:24 +0500

    Create sw01_azure-prov.ino

---
+++ examples/CW02_instr_test/sw01_azure-prov.ino
@@ -0,0 +1,34 @@
+#include "xInstrument.h"
+#include "xSW01.h"
+
+
+double tempC, press, hum;
+xInstrument instr;
+xSW01 SW01;
+
+void setup() {
+  // put your setup code here, to run once
+
+  instr.createVariables(3, "temp", "pressure","humidity");
+  instr.enableLEDS();
+  instr.enableWiFi();
+  instr.enableAzure();
+  instr.begin();
+  Wire.begin();
+  SW01.begin();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  SW01.poll();
+  
+  tempC = SW01.getTempC();
+  press = SW01.getPressure();
+  hum = SW01.getHumidity();
+
+  instr.updateVariables(3, tempC, press, hum);
+  instr.loop();
+  
+
+}
